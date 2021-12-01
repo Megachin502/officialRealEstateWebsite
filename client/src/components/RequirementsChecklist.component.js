51c7bb5e255b30d1b67ed79d72fd72b5f9ff2cff
Elvis Lam <elvislam502@gmail.com>
2021-11-30 23:43:08 -0800

    Update RequirementsChecklist.component.js

--- client/src/components/RequirementsChecklist.component.js
+++ client/src/components/RequirementsChecklist.component.js
@@ -6,7 +6,7 @@ export default function RequirementsChecklist() {
             <iframe title="r" className="borderBottom" src="https://docs.google.com/document/d/e/2PACX-1vTXP6sDnlFnQkR6zzJvZ0q9Wvq46yNT0_nLusmHnmTajxuu9wxZhDQ3te5s6rt7xmnYtK3Z-ii6HWye/pub?embedded=true" width="100%" height="600px"></iframe>
             <a rel="noreferrer" target="_blank" href="https://docs.google.com/document/d/e/2PACX-1vTXP6sDnlFnQkR6zzJvZ0q9Wvq46yNT0_nLusmHnmTajxuu9wxZhDQ3te5s6rt7xmnYtK3Z-ii6HWye/pub">Requirements Checklist</a>
             <ClientOnboardForm />
-
+            </br>
         </div>
     )
 }
