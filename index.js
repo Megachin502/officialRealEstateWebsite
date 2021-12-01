6a497f1a39ef984cc87523e5e4183be20b66488c
Elvis Lam <elvislam502@gmail.com>
2021-11-30 17:58:42 -0800

    a

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 //Setting up the app stuff
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
